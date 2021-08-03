79fca6299904994cee28ce83925c50bd92a5b84b
sanukumar <sanukumar1138@gmail.com>
2021-08-03 11:20:35 +0530

    articl implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import MainArticle from './components/mainArticle';
+import Header from './components/header';
+import axios from 'axios';
+import { API_KEY } from './token';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [articleData, setArticleData] = useState([]);
+  const [articleDup, setArticleDup] = useState([]);
+
+  useEffect(() => {
+    fetchArticleData();
+  }, []);
+
+  const fetchArticleData = async () => {
+    const { data } = await axios.get(
+      `https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=${API_KEY}`
+    );
+    setArticleData(data.results);
+    setArticleDup(data.results);
+  };
+
+  const handleSearch = (data) => {
+    if (data) {
+      let searchedData = articleDup.filter((a) =>
+        a.byline.toLowerCase().includes(data.toLowerCase())
+      );
+      setArticleData(searchedData);
+    } else {
+      setArticleData(articleDup);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header searchData={handleSearch} />
+      <MainArticle articleData={articleData} />
+    </>
   );
-}
+};
 
 export default App;
